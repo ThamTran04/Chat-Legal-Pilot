324efbed0e116b96b0439d5204ed9f6400ade24e
ThamTran <thihanhtham.tran@gmail.com>
2021-12-18 23:48:03 +0100

    symfony: users fixtures

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordHasherInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    private function _createUser(
+        ObjectManager $manager,
+        string $nom,
+        string $prenom,
+        string $email,
+        array $roles,
+        string $agent
+    ): void {
+        $user = new User();
+        $user->setNom($nom)
+            ->setPrenom($prenom)
+            ->setEmail($email)
+            ->setPassword($this->userPasswordEncoder->hashPassword($user, "1234"))
+            ->setAgent($agent)
+            ->setRoles($roles);
+        $manager->persist($user);
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // creating an admin
+        $this->_createUser($manager, "Henry", "Erwan", "admin@gmail.com", ["admin", "Senior"], "Sophia");
+
+        // creating others users
+        for ($i = 1; $i <= 3; $i++) {
+            $this->_createUser($manager, "nom$i", "prenom$i", "user$i@addixware.fr", ["Junior"], "Aix");
+        }
 
         $manager->flush();
     }
